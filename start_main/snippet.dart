123e8dd8e13dc20b4b0341da5db0e286114a40fd
Kirill Stativkin <kirill.stativkin@cellenza.com>
2021-10-03 22:00:37 +0200

    update snippet

--- start_main/snippet.dart
+++ start_main/snippet.dart
@@ -1,22 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-
-namespace MyFirstCoreApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+void main() {
+  print("Hello World!");
 }
